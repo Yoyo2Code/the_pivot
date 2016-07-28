f4bb5dda737798e948db32680d530e94daf47fae
Trent Whitinger <twhitinger@gmail.com>
2016-07-27 18:48:21 -0600

    user can login and checkout closes #12

---
+++ spec/features/user_can_login_checkout_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.feature "user can login and checkout" do
+  scenario "user can see checkout and items" do
+    # As a visitor when I have items in my cart
+    # And when I visit "/cart"
+    visit cart_index_path
+
+    # I should not see an option to "Checkout"
+    expect(page).to_not have_link("Checkout")
+    # I should see an option to "Login or Create Account to Checkout"
+    expect(page).to have_content("Login or Create Account to Checkout")
+    # After I create an account
+    click_on("Create Account")
+    # And I fill in my desired credentials
+    fill_in "Username", with: "Penelope"
+    fill_in "Password", with: "password"
+    click_on("Create Account")
+    # And I visit "/cart
+    visit cart_index_path
+    # Then I should see all of the data that was there when I was not logged in
+    expect(page).to have_link("Checkout")
+    # When I click "Logout"
+
+    click_on("Logout")
+    # Then I should see see "Login"
+    expect(page).to have_link("Login")
+    # And I should not see "Logout"
+    expect(page).to_not have_link("Logout")
+  end
+end
