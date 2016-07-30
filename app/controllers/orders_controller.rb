7d6e57eb9fc65f1b1c417469d8cdc58a41969f31
Trent Whitinger <twhitinger@gmail.com>
2016-07-30 17:14:17 -0600

    order history display quantity

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -11,7 +11,6 @@ class OrdersController < ApplicationController
     @cart.find_items.each do |item|
       order.order_items.create(item_id: item.id, title: item.title, quantity: @cart.contents[item.id.to_s], price: item.price)
     end
-    # order.order_items << @cart.find_items
     @cart.contents.clear
     flash[:notice] = "Order Successfully Placed!"
     redirect_to orders_path
@@ -20,16 +19,9 @@ class OrdersController < ApplicationController
   def show
     @order_number = @_request.env["REQUEST_PATH"].split("/").last
     current_user.orders.find(@order_number)
-
     @order = current_user.orders.find(@order_number)
 
-
   end
-
-  # def get_quantity(item_number)
-  #   item = @order.order_items.find(item_number)
-  #       @order_items = @cart.contents
-  # end
 
 
 end
