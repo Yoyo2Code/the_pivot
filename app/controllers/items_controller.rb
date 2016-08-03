f9d1c20108a973cd348bf998f2da060df882fcb8
Caleb <calebcowen@gmail.com>
2016-08-03 14:33:26 -0600

    add functionality admin can create items and create or add item to category on creation

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,12 +6,32 @@ class ItemsController < ApplicationController
   end
 
   def show
-    if @_request.env["HTTP_REFERER"].split("/")[3] && @_request.env["HTTP_REFERER"].split("/") == "items" || @_request.env["HTTP_REFERER"].split("/").count == 3
-      @item = Item.find(params[:id])
-    else
+    if @_request.env["HTTP_REFERER"].split("/") && @_request.env["HTTP_REFERER"].split("/")[3] == "orders"
       order_item = OrderItem.find(params[:id])
       @item = Item.find(order_item.item_id)
+    else
+      @item = Item.find(params[:id])
     end
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    category = Category.find_or_create_by(title: params[:item][:category].downcase)
+    item = category.items.create(item_params)
+    if item.save
+      redirect_to item_path(item)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:title, :description, :price, :image_path)
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160801145252) do
+ActiveRecord::Schema.define(version: 20160803191950) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,11 +25,12 @@ ActiveRecord::Schema.define(version: 20160801145252) do
     t.string   "title"
     t.text     "description"
     t.decimal  "price"
-    t.string   "image_path"
+    t.string   "image_path",         default: "http://vignette4.wikia.nocookie.net/lego/images/6/63/New52Cyborg_Happy!.png/revision/latest?cb=20141224001316"
     t.integer  "category_id"
-    t.datetime "created_at",              null: false
-    t.datetime "updated_at",              null: false
-    t.integer  "status",      default: 0
+    t.datetime "created_at",                                                                                                                                   null: false
+    t.datetime "updated_at",                                                                                                                                   null: false
+    t.integer  "status",             default: 0
+    t.string   "image_content_type"
   end
 
   create_table "order_items", force: :cascade do |t|
