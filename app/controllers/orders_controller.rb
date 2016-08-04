2e482a1ce0d1d6ed3ce9c949c43925386e486a7c
Caleb <calebcowen@gmail.com>
2016-08-04 09:54:37 -0600

    another bug

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,6 @@
 class OrdersController < ApplicationController
   skip_before_action :require_admin
-  skip_before_action :require_user, only: [:show]
+  skip_before_action :require_user, only: [:show, :destroy]
 
   def index
     @orders = current_user.orders
