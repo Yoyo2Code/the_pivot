a82b3f4b34d4adef1f8c0aa9dbb0a3ec113c3a73
David Tinianow <david.tinianow@gmail.com>
2016-08-17 13:15:43 -0600

    Change property attributes Remove status from properties Rename properties price to price per guest Add max occupancy to properties

--- spec/models/property_spec.rb
+++ spec/models/property_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
-RSpec.describe Item, type: :model do
+RSpec.describe Property, type: :model do
   context "validations" do
     it { is_expected.to validate_presence_of(:title) }
     it { is_expected.to validate_presence_of(:description) }
-    it { is_expected.to validate_presence_of(:price) }
+    it { is_expected.to validate_presence_of(:price_per_guest) }
     it { is_expected.to validate_presence_of(:image_path) }
     it { is_expected.to validate_uniqueness_of(:title) }
     it { is_expected.to belong_to(:location) }
-    it { should have_many(:order_items) }
-    it { should have_many(:orders).through(:order_items) }
+    it { should have_many(:reservations) }
+    it { should have_many(:orders).through(:reservations) }
   end
 end
