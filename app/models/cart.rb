b2b383bc7e017f24fba609ff15159fcf6c9fc84d
Automatic365 <jason.hanna01@gmail.com>
2016-08-20 17:06:23 -0600

    closes #10 (#48)
    * add multiple items to cart functionality

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -4,15 +4,12 @@ class Cart
 
   def initialize(contents)
     @contents = contents || {}
-    # @starting_date = contents[:start_date]
-    # @end_date = contents[:end_date]
-    # @occupancy = contents[:occupancy]
   end
 
   def add_item(params)
     contents[params[:property_id].to_s] = { starting_date: params[:starting_date],
-                                            end_date: params[:end_date],
-                                            occupancy: params[:occupancy] }
+      end_date: params[:end_date],
+      occupancy: params[:occupancy] }
   end
 
   def count_of(item_id)
@@ -29,17 +26,10 @@ class Cart
     end
   end
 
-  # def find_items
-  #   contents.keys.map do |id|
-  #     Property.find(id)
-  #   end
-  # end
-
   def total_price
-    contents.map do |id, quantity|
-      item = Property.find(id)
-      item.price * quantity
-    end.sum.to_f
+    find_items.map do |cart_item|
+      cart_item.subtotal
+    end.sum
   end
 
   def delete_item(item_id)
@@ -50,5 +40,4 @@ class Cart
   def increment_quantity_by
     content[item_id.to_s] += update
   end
-
 end
