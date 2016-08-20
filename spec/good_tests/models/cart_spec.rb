5e0e552ca0b91f3790a3ce34ca31b2eff0222d37
David Tinianow <david.tinianow@gmail.com>
2016-08-20 15:02:17 -0600

    Fix hound errors

--- spec/good_tests/models/cart_spec.rb
+++ spec/good_tests/models/cart_spec.rb
@@ -2,13 +2,13 @@ require 'rails_helper'
 
 RSpec.describe "Cart" do
   it "should have initial content" do
-    cart = Cart.new( "1" =>  { starting_date: "08/15/2016",
-                               end_date: "09/01/2016",
-                               occupancy: 2 } )
+    cart = Cart.new("1" =>  { starting_date: "08/15/2016",
+                              end_date: "09/01/2016",
+                              occupancy: 2 })
 
-    expect(cart.contents).to eq( "1" => { starting_date: "08/15/2016",
-                                          end_date: "09/01/2016",
-                                          occupancy: 2 } )
+    expect(cart.contents).to eq("1" => { starting_date: "08/15/2016",
+                                         end_date: "09/01/2016",
+                                         occupancy: 2 })
   end
 
   it "should have an empty hash as initial contents if no session back" do
@@ -28,27 +28,30 @@ RSpec.describe "Cart" do
       business_id: business.id,
       max_occupancy: 3
     )
-    params = { property_id: property.id,
-               occupancy: 2,
-               starting_date: "08/15/2016",
-               end_date: "09/01/2016" }
+    params = {
+      property_id: property.id,
+      occupancy: 2,
+      starting_date: "08/15/2016",
+      end_date: "09/01/2016"
+    }
+
     cart = Cart.new(nil)
 
     cart.add_item(params)
 
-    expect(cart.contents).to eq( "#{property.id}" =>
+    expect(cart.contents).to eq("#{property.id}" =>
                                { starting_date: "08/15/2016",
                                  end_date: "09/01/2016",
-                                 occupancy: 2 } )
+                                 occupancy: 2 })
   end
-  
+
   it "should return total number of items in cart" do
-    cart = Cart.new( "1" =>  { starting_date: "08/15/2016",
-      end_date: "09/01/2016",
-      occupancy: 2 } )
-      cart.add_item( "2" =>  { starting_date: "08/16/2016",
-        end_date: "09/10/2016",
-        occupancy: 4 } )
+    cart = Cart.new("1" => { starting_date: "08/15/2016",
+                             end_date: "09/01/2016",
+                             occupancy: 2 })
+    cart.add_item("2" => { starting_date: "08/16/2016",
+                           end_date: "09/10/2016",
+                           occupancy: 4 })
 
         expect(cart.total).to eq(2)
       end
@@ -65,7 +68,7 @@ RSpec.describe "Cart" do
     item = cat1.items.create!(title: "Robot Arm", description: "Cool ass arm", price: 10000.0, image_path: 'http://img09.deviantart.net/588b/i/2004/272/7/2/i__robot_arm_by_chainsawdeathriot.jpg')
 
 
-    cart = Cart.new({ item.id => 3})
+    cart = Cart.new({ item.id => 3 })
 
     expect(cart.find_items.first).to eq(Item.first)
   end
@@ -90,8 +93,5 @@ RSpec.describe "Cart" do
     cart.delete_item(item.id)
 
     expect(cart.contents[item.id.to_s]).to eq(2)
-
-
   end
-
 end
