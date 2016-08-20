5ff42bea0cb99cbc0863908bba59766d63187ae4
Jason Hanna <jason.hanna01@gmail.com>
2016-08-20 16:53:29 -0600

    add multiple items to cart functionality

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -4,43 +4,40 @@ class Cart
 
   def initialize(contents)
     @contents = contents || {}
-    # @starting_date = contents[:start_date]
-    # @end_date = contents[:end_date]
-    # @occupancy = contents[:occupancy]
   end
 
   def add_item(params)
     contents[params[:property_id].to_s] = { starting_date: params[:starting_date],
       end_date: params[:end_date],
       occupancy: params[:occupancy] }
-    end
+  end
 
-    def count_of(item_id)
-      contents[item_id.to_s]
-    end
+  def count_of(item_id)
+    contents[item_id.to_s]
+  end
 
-    def total
-      contents.keys.count
-    end
+  def total
+    contents.keys.count
+  end
 
-    def find_items
-      contents.map do |id, data|
-        CartItem.new(id, data["occupancy"], data["starting_date"], data["end_date"])
-      end
-    end
-
-    def total_price
-      find_items.map do |cart_item|
-        cart_item.subtotal
-      end.sum
-    end
-
-    def delete_item(item_id)
-      contents[item_id.to_s] -= 1
-      contents.delete_if { |id, quantity| quantity == 0 }
-    end
-
-    def increment_quantity_by
-      content[item_id.to_s] += update
+  def find_items
+    contents.map do |id, data|
+      CartItem.new(id, data["occupancy"], data["starting_date"], data["end_date"])
     end
   end
+
+  def total_price
+    find_items.map do |cart_item|
+      cart_item.subtotal
+    end.sum
+  end
+
+  def delete_item(item_id)
+    contents[item_id.to_s] -= 1
+    contents.delete_if { |id, quantity| quantity == 0 }
+  end
+
+  def increment_quantity_by
+    content[item_id.to_s] += update
+  end
+end
