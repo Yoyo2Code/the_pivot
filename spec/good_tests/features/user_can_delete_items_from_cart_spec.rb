86aaf0ee889744fd38e696df6d98136c0de142f6
Yoseph Anderson <yrganderson@gmail.com>
2016-08-20 19:00:51 -0600

    remove from cart * hound

--- spec/good_tests/features/user_can_delete_items_from_cart_spec.rb
+++ spec/good_tests/features/user_can_delete_items_from_cart_spec.rb
@@ -2,17 +2,19 @@ require 'rails_helper'
 
 RSpec.feature 'user can delete items from cart' do
   scenario 'delete item from a cart with 1 item' do
-
     business = create(:business)
 
-    location = create(:location) do |location|
-      location.properties.create(attributes_for(:property,
+    location = create(:location) do |loc|
+      loc.properties.create(attributes_for(:property,
                                            business_id: business.id))
     end
 
-    page.set_rack_session(cart: { Property.first.id.to_s => { starting_date: "08/15/2016",
-                          end_date: "09/01/2016",
-                          occupancy: 2 }})
+    page.set_rack_session(
+                          cart:
+                            { Property.first.id.to_s =>
+                              { starting_date: "08/15/2016",
+                                end_date: "09/01/2016",
+                                occupancy: 2 }})
 
     visit cart_path
 
@@ -27,55 +29,55 @@ RSpec.feature 'user can delete items from cart' do
     expect(page).to have_content("Your Cart is Empty")
   end
 
-    scenario 'user can delete multiple items from cart'do
+  scenario 'user can delete multiple items from cart' do
+    business = create(:business)
 
-      business = create(:business)
+    location = create(:location) do |loc|
+      loc.properties.create(attributes_for(:property,
+                                           business_id: business.id))
 
-      location = create(:location) do |location|
-        location.properties.create(attributes_for(:property,
-                                             business_id: business.id))
+      loc.properties.create(attributes_for(:property, title: "Disneyland",
+                                           business_id: business.id))
+    end
 
-        location.properties.create(attributes_for(:property, title: "Disneyland",
-                                             business_id: business.id))
-      end
+    property_1 = Property.first
+    property_2 = Property.second
 
-      property_1 = Property.first
-      property_2 = Property.second
+    page.set_rack_session(
+                          cart:
+                            { property_1.id.to_s =>
+                              { starting_date: "08/15/2016",
+                                end_date: "09/01/2016",
+                                occupancy: 2 },
+                              property_2.id.to_s =>
+                              { starting_date: "08/15/2016",
+                                end_date: "09/01/2016",
+                                occupancy: 2 }
+                            }
+                        )
 
-      page.set_rack_session(cart:
-                                { property_1.id.to_s =>
-                                  { starting_date: "08/15/2016",
-                                    end_date: "09/01/2016",
-                                    occupancy: 2 },
-                                  property_2.id.to_s =>
-                                  { starting_date: "08/15/2016",
-                                    end_date: "09/01/2016",
-                                    occupancy: 2 }
-                                }
-                            )
+    visit cart_path
 
-      visit cart_path
+    expect(page).to have_content("Trump Tower")
+    expect(page).to have_content("Disneyland")
+    expect(page).to have_content("Cart Items: 2")
 
-      expect(page).to have_content("Trump Tower")
-      expect(page).to have_content("Disneyland")
-      expect(page).to have_content("Cart Items: 2")
+    within("#cart-item-#{property_1.id}") do
+      click_button "Remove"
+    end
 
-      within("#cart-item-#{property_1.id}") do
-        click_button "Remove"
-      end
+    expect(page).to have_content("Disneyland")
+    expect(page).to have_no_content("Trump Tower")
+    expect(page).to have_content("Cart Items: 1")
+    expect(page).to have_no_content("Your Cart is Empty")
 
-      expect(page).to have_content("Disneyland")
-      expect(page).to have_no_content("Trump Tower")
-      expect(page).to have_content("Cart Items: 1")
-      expect(page).to have_no_content("Your Cart is Empty")
+    within("#cart-item-#{property_2.id}") do
+      click_button "Remove"
+    end
 
-      within("#cart-item-#{property_2.id}") do
-        click_button "Remove"
-      end
-
-      expect(page).to have_no_content("Disneyland")
-      expect(page).to have_no_content("Trump Tower")
-      expect(page).to have_content("Cart Items: 0")
-      expect(page).to have_content("Your Cart is Empty")
+    expect(page).to have_no_content("Disneyland")
+    expect(page).to have_no_content("Trump Tower")
+    expect(page).to have_content("Cart Items: 0")
+    expect(page).to have_content("Your Cart is Empty")
   end
 end
