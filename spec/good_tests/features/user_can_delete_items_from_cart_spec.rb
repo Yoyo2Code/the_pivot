6654b495a89bb1787bac989ec4edcf4401965a43
Yoseph Anderson <yrganderson@gmail.com>
2016-08-20 18:49:26 -0600

    card #46 * user can remove multiple items from cart

--- spec/good_tests/features/user_can_delete_items_from_cart_spec.rb
+++ spec/good_tests/features/user_can_delete_items_from_cart_spec.rb
@@ -8,10 +8,11 @@ RSpec.feature 'user can delete items from cart' do
     location = create(:location) do |location|
       location.properties.create(attributes_for(:property,
                                            business_id: business.id))
-      page.set_rack_session(cart: { Property.first.id.to_s => { starting_date: "08/15/2016",
-                                end_date: "09/01/2016",
-                                occupancy: 2 }})
     end
+
+    page.set_rack_session(cart: { Property.first.id.to_s => { starting_date: "08/15/2016",
+                          end_date: "09/01/2016",
+                          occupancy: 2 }})
 
     visit cart_path
 
@@ -23,18 +24,58 @@ RSpec.feature 'user can delete items from cart' do
 
     expect(page).to have_content("Successfully removed")
     expect(page).to have_content("Cart Items: 0")
+    expect(page).to have_content("Your Cart is Empty")
   end
 
-    xscenario 'user can delete multiple items from cart'do
-    location = create(:location) do |location|
-      location.items.create(attributes_for(:property))
-    end
+    scenario 'user can delete multiple items from cart'do
 
-    visit items_path
-    click_on("Add to Cart")
-    click_on("Add to Cart")
-    click_on("Add to Cart")
+      business = create(:business)
 
-    expect(page).to have_content("Cart Items: 3")
+      location = create(:location) do |location|
+        location.properties.create(attributes_for(:property,
+                                             business_id: business.id))
+
+        location.properties.create(attributes_for(:property, title: "Disneyland",
+                                             business_id: business.id))
+      end
+
+      property_1 = Property.first
+      property_2 = Property.second
+
+      page.set_rack_session(cart:
+                                { property_1.id.to_s =>
+                                  { starting_date: "08/15/2016",
+                                    end_date: "09/01/2016",
+                                    occupancy: 2 },
+                                  property_2.id.to_s =>
+                                  { starting_date: "08/15/2016",
+                                    end_date: "09/01/2016",
+                                    occupancy: 2 }
+                                }
+                            )
+
+      visit cart_path
+
+      expect(page).to have_content("Trump Tower")
+      expect(page).to have_content("Disneyland")
+      expect(page).to have_content("Cart Items: 2")
+
+      within("#cart-item-#{property_1.id}") do
+        click_button "Remove"
+      end
+
+      expect(page).to have_content("Disneyland")
+      expect(page).to have_no_content("Trump Tower")
+      expect(page).to have_content("Cart Items: 1")
+      expect(page).to have_no_content("Your Cart is Empty")
+
+      within("#cart-item-#{property_2.id}") do
+        click_button "Remove"
+      end
+
+      expect(page).to have_no_content("Disneyland")
+      expect(page).to have_no_content("Trump Tower")
+      expect(page).to have_content("Cart Items: 0")
+      expect(page).to have_content("Your Cart is Empty")
   end
 end
