7cf583a59958cb2484b177943d5d7de413aee2b7
Jason Hanna <jason.hanna01@gmail.com>
2016-08-22 15:25:12 -0600

    add business status to business table

---
+++ spec/good_tests/features/business_admin_can_update_their_business_information_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+RSpec.feature "business admin can update their own business info" do
+  xscenario "they can edit and change business name" do
+
+  end
+end
--- spec/good_tests/models/business_spec.rb
+++ spec/good_tests/models/business_spec.rb
@@ -5,10 +5,15 @@ RSpec.describe Business, type: :model do
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:slug) }
     it { should validate_uniqueness_of(:name) }
+    it { should validate_presence_of(:status) }
   end
 
   context 'associations' do
     it { should have_many(:business_roles) }
     it { should have_many(:roles).through(:business_roles) }
+  end
+
+  context 'enum' do
+    it { should define_enum_for(:status) }
   end
 end
