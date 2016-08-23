a4102c6f9ac5678d600e979df4ec775fcbcf23e3
David Tinianow <david.tinianow@gmail.com>
2016-08-23 16:42:06 -0600

    Business admin can edit properties (#81)
    * Business admin can update business info
    * Business admins can cancel an edit and return to dashboard
    * User can click edit on their property
    * Business admin can update a property
    * Move business admin functionality into admin controller
    * Create admin businesses controller
    * Create admin properties controller

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,22 +1,22 @@
 class Admin::UsersController < Admin::BaseController
-  def show
-    @user = current_user
-  end
-
-  def edit
-    @user = User.find(params[:id])
-  end
-
-  def update
-    @user = User.find(params[:id])
-    if @user.update(user_params)
-      redirect_to admin_dashboard_path
-    else
-      render :edit
-    end
-  end
-
-  def user_params
-    params.require(:user).permit(:username, :password)
-  end
+  # def show
+  #   @user = current_user
+  # end
+  #
+  # def edit
+  #   @user = User.find(params[:id])
+  # end
+  #
+  # def update
+  #   @user = User.find(params[:id])
+  #   if @user.update(user_params)
+  #     redirect_to admin_dashboard_path
+  #   else
+  #     render :edit
+  #   end
+  # end
+  #
+  # def user_params
+  #   params.require(:user).permit(:username, :password)
+  # end
 end
