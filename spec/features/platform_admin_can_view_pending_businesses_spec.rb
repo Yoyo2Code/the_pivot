d8bbe4e398f1269d3e5dbfe0f17e9d7d23bd7add
David Tinianow <david.tinianow@gmail.com>
2016-08-24 10:28:24 -0600

    Admin can approve business (#91)
    * Setup test
    
    * Platform admin can see pending businesses
    
    * See message if there are no pending businesses
    
    * Admin can approve creation of business
    
    * Platform admin can approve or deny businesses
    
    * Change approve to activate
    
    * Restructure tests
    
    * Comment out test

--- spec/features/platform_admin_can_view_pending_businesses_spec.rb
+++ spec/features/platform_admin_can_view_pending_businesses_spec.rb
@@ -1,12 +1,4 @@
 require 'rails_helper'
-# As a User,
-# When I visit my dashboard
-# and I click 'Open a Business'
-# and I fill in my business name and description
-# and I click 'Submit'
-# then I expect to be redirected to my dashboard,
-# and I expect to see a flash message confirming /
-# that my application has been submitted.
 
 RSpec.describe 'Platform admin can view pending businesses', type: :feature do
   scenario 'they click a link on their dashboard and see pending businesses' do
@@ -20,11 +12,6 @@ RSpec.describe 'Platform admin can view pending businesses', type: :feature do
     click_button 'Submit New Business Application'
 
     user = create(:user, username: "David", role: 2)
-    # business = create(:business, user: user)
-    # location = create(:location)
-    # create(:property, business: business, location: location)
-    # property = location.properties.first
-    # business_name = property.business.slug
     page.set_rack_session(user_id: user.id)
 
     visit dashboard_path
@@ -35,5 +22,22 @@ RSpec.describe 'Platform admin can view pending businesses', type: :feature do
     expect(page).to have_css("img[src*='http://people.turing.io/assets/students/dj_greenfield-572acd74ca15fcc9eff531f092d5e234.jpg']")
     expect(page).to have_button "Approve"
     expect(page).to have_button "Deny"
+  end
+
+  scenario "there are no pending businesses and they see a message" do
+    user = create(:user, username: "David", role: 2)
+    page.set_rack_session(user_id: user.id)
+
+    visit dashboard_path
+
+    click_on "View Pending Businesses"
+
+    expect(page).to have_content 'There are no pending businesses.'
+
+    within('.return-to-dashboard') do
+      click_on "Return to My Dashboard"
+    end
+
+    expect(current_path).to eq dashboard_path
   end
 end
