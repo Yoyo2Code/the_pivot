017f8777c0248237d01cb5e77b9e0e955941b0f6
David Tinianow <david.tinianow@gmail.com>
2016-08-24 16:43:27 -0600

    Eliminate comments

--- app/controllers/platform_admin/businesses_controller.rb
+++ app/controllers/platform_admin/businesses_controller.rb
@@ -4,35 +4,12 @@ class PlatformAdmin::BusinessesController < PlatformAdmin::BaseController
     @live_businesses = Business.active_or_inactive?
   end
 
-  # def pending
-  #   @businesses = Business.where(status: 'pending')
-    # params[:filter] # 'pending'
-    # @partial = "#{params[:filter]}_businesses"
-    # # /businesses?status=inactive&status=active
-  # end
-
-  # def manage
-  #   @businesses = Business.where(status: ['active', 'inactive'])
-  # end
-
   def update
     business = Business.find(params[:id])
     business.update(status: statuses[params[:filter]])
-    # if params[:filter] == "activate"
-    #   business.update(status: 'active')
-    # elsif params[:filter] == "deactivate"
-    #   business.update(status: 'inactive')
-    # end
     flash[:success] = "Business has been #{params[:filter]}d!"
     redirect_to platform_admin_businesses_path
   end
-  #
-  # def destroy
-  #   business = Business.find(params[:id])
-  #   business.destroy
-  #   flash[:success] = "Business successfully deleted."
-  #   redirect_to businesses_pending_path
-  # end
 
   def destroy
     business = Business.find(params[:id])
